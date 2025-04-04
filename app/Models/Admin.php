4982ffe41d2088edf99e22c8d720b3a368c122d7
Saral-Chauhangit <saralchauhan424t@gmail.com>
2025-04-04 11:27:08 +0530

    refactor(authorization): simplify permission checks and gate definitions
    Extract the super admin check into a separate method `isAdmin` in the `Admin` model for better readability and maintainability. Replace individual gate definitions with a single `Gate::before` callback to handle permission checks dynamically, reducing redundancy and improving code efficiency.

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -18,14 +18,19 @@ class Admin extends Authenticatable
     public function role(){
         return $this->belongsTo(Role::class,'role_id');
     }
+    public function isAdmin(){
+         return $this->role->permissions()->where('permission', 'super admin')->exists();
+    }
     public function hasPermission($permission){
-        if ($this->role->permissions()->where('permission', 'super admin')->exists()) {
+        if ($this->isAdmin()) {
             return true;
         }
         // Check if the admin's role has the requested permission
         // dd($permission);
         if($this->role) {
-            return $this->role->permissions()->where('permission', $permission)->exists();
+            return $this->role->wherehas('permissions', function ($query) use ($permission) {
+                $query->where('permission', $permission);
+            })->exists();
         }
         
         return false;
--- app/Providers/AuthorizeServiceProvider.php
+++ app/Providers/AuthorizeServiceProvider.php
@@ -20,42 +20,46 @@ class AuthorizeServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        
-        Gate::define('manage_categories',function($admin){
-            return $admin->hasPermission('Categories');
+        Gate::before(function($admin,$permission){
+            $convertedPermission = ucfirst(str_replace('manage_', '', $permission));
+            // dd($convertedPermission);
+            return $admin->hasPermission($convertedPermission); //if admin has permission then return tru
         });
+        // Gate::define('manage_categories',function($admin){
+        //     return $admin->hasPermission('Categories');
+        // });
         
-        Gate::define('manage_products',function($admin){
-            return $admin->hasPermission('Products');
-        });
+        // Gate::define('manage_products',function($admin){
+        //     return $admin->hasPermission('Products');
+        // });
         
-        Gate::define('manage_orders',function($admin){
-            return $admin->hasPermission('Orders');
-        });
+        // Gate::define('manage_orders',function($admin){
+        //     return $admin->hasPermission('Orders');
+        // });
         
-        Gate::define('manage_customers',function($admin){
-            return $admin->hasPermission('Customers');
-        });
+        // Gate::define('manage_customers',function($admin){
+        //     return $admin->hasPermission('Customers');
+        // });
         
-        Gate::define('manage_admins',function($admin){
-            return $admin->hasPermission('Admins');
-        });
+        // Gate::define('manage_admins',function($admin){
+        //     return $admin->hasPermission('Admins');
+        // });
         
-        Gate::define('manage_roles',function($admin){
-            return $admin->hasPermission('Roles');
-        });
+        // Gate::define('manage_roles',function($admin){
+        //     return $admin->hasPermission('Roles');
+        // });
         
-        Gate::define('manage_permissions',function($admin){
-            return $admin->hasPermission('Permissions');
-        });
+        // Gate::define('manage_permissions',function($admin){
+        //     return $admin->hasPermission('Permissions');
+        // });
         
-        Gate::define('manage_static_blocks',function($admin){
-            return $admin->hasPermission('Static Blocks');
-        });
+        // Gate::define('manage_static_blocks',function($admin){
+        //     return $admin->hasPermission('Static Blocks');
+        // });
         
-        Gate::define('manage_static_pages',function($admin){
-            return $admin->hasPermission('Static Pages');
-        });
+        // Gate::define('manage_static_pages',function($admin){
+        //     return $admin->hasPermission('Static Pages');
+        // });
     
     }
 }
